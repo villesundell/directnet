08bf130900986e2c2b5586120a76350668dbbc3f
cdgregorr <>
2004-11-10 02:16:51 +0000

    Removing accidental files
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk/directnet@7 40aaf297-f90e-0410-8da6-8adc2782f1e2

--- tests/mkfnd.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("fnd%c%cBlah01;SuperMan;;", 1, 1);
-    fflush(stdout);
-    system("gpg --homedir ~/.dntest -a --export Testman --no-tty");
-    putchar('\0');
-}
