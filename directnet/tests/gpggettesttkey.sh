fc949610826f37f71fa517d8dd47b65d4367325c
cdgregorr <>
2004-11-10 02:16:51 +0000

    Removing accidental files
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk@7 40aaf297-f90e-0410-8da6-8adc2782f1e2

--- directnet/tests/gpggettesttkey.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gpg --homedir ~/.dntest --delete-key Testt
-gpg --homedir ~/.directnet -a --export Testt | gpg --homedir ~/.dntest --import
--- directnet/tests/mkkey.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("key%c%cTestMan;", 1, 1);
-    fflush(stdout);
-    system("gpg --homedir ~/.dntest -a --export Testman --no-tty");
-    putchar('\0');
-}
