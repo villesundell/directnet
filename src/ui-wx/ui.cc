1849193c5e2c97bbb8a23c206802d17b341a925a
cdgregorr <>
2004-11-18 05:52:10 +0000

    Adding tiny bits of wxWidgets supports.
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk/directnet@53 40aaf297-f90e-0410-8da6-8adc2782f1e2

---
+++ src/ui-wx/ui.cc
@@ -0,0 +1,82 @@
+/*
+ * hworld.cpp
+ * Hello world sample by Robert Roebling
+ */
+
+#include "wx/wx.h" 
+
+class MyApp: public wxApp
+{
+    virtual bool OnInit();
+};
+
+class MyFrame: public wxFrame
+{
+public:
+
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+    void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+
+    DECLARE_EVENT_TABLE()
+};
+
+enum
+{
+    ID_Quit = 1,
+    ID_About,
+};
+
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(ID_Quit, MyFrame::OnQuit)
+    EVT_MENU(ID_About, MyFrame::OnAbout)
+END_EVENT_TABLE()
+
+IMPLEMENT_APP_NO_MAIN(MyApp)
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame( "Hello World", wxPoint(50,50), wxSize(450,340) );
+    frame->Show(TRUE);
+    SetTopWindow(frame);
+    return TRUE;
+} 
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+: wxFrame((wxFrame *)NULL, -1, title, pos, size)
+{
+    wxMenu *menuFile = new wxMenu;
+
+    menuFile->Append( ID_About, "&About..." );
+    menuFile->AppendSeparator();
+    menuFile->Append( ID_Quit, "E&xit" );
+
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append( menuFile, "&File" );
+
+    SetMenuBar( menuBar );
+
+    CreateStatusBar();
+    SetStatusText( "Welcome to wxWindows!" );
+}
+
+void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    Close(TRUE);
+}
+
+void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+    wxMessageBox("This is a wxWindows Hello world sample",
+        "About Hello World", wxOK | wxICON_INFORMATION, this);
+}
+
+int uiInit()
+{
+    wxEntry();
+}
+
+void uiDispMsg(char *from, char *msg)
+{
+}
