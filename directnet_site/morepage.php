4e514eddd2c405726543dcff73ab2c025e0f73de
cdgregorr <>
2004-12-16 03:52:32 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk@94 40aaf297-f90e-0410-8da6-8adc2782f1e2

--- directnet_site/morepage.php
+++ directnet_site/morepage.php
@@ -68,46 +68,46 @@ function morepage($page, $frame) {
         // GNU/Linux
         print "<li>GNU/Linux<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-i686-pc-linux-gnu.gz'>i686 (Intel/AMD PC)</a></li>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-alphaev67-unknown-linux.gz'>Alpha</a></li>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-x86_64-unknown-linux-gnu.gz'>x86_64 (AMD AMD64/Intel EM64T PC)</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-i686-pc-linux-gnu.gz?download'>i686 (Intel/AMD PC)</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-alphaev67-unknown-linux.gz?download'>Alpha</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-x86_64-unknown-linux-gnu.gz?download'>x86_64 (AMD AMD64/Intel EM64T PC)</a></li>";
         print "</ul></li>";
         
         // OSX
         print "<li>Mac OS X (10.2 or higher)<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-powerpc-apple-darwin6.8.gz'>PPC</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-powerpc-apple-darwin6.8.gz?download'>PPC</a></li>";
         print "</ul></li>";
         
         // Solaris
         print "<li>Solaris 9<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-sparc-sun-solaris2.9.gz'>ULTRASPARC</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-sparc-sun-solaris2.9.gz?download'>ULTRASPARC</a></li>";
         print "</ul></li>";
         
         // FreeBSD
         print "<li>FreeBSD 4.10<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-i386-unknown-freebsd4.10.gz'>i686 (Intel/AMD PC)</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-i386-unknown-freebsd4.10.gz?download'>i686 (Intel/AMD PC)</a></li>";
         print "</ul></li>";
         
         // HP-UX
         print "<li>HP-UX 11<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-hppa2.0w-hp-hpux11.23.gz'>HPPA</a></li>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-ia64-hp-hpux11.23.gz'>Itanium</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-hppa2.0w-hp-hpux11.23.gz?download'>HPPA</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-ia64-hp-hpux11.23.gz?download'>Itanium</a></li>";
         print "</ul></li>";
         
         // HP Tru64
         print "<li>HP Tru64 UNIX<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-alphaev7-dec-osf5.1b.gz'>Alpha</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-alphaev7-dec-osf5.1b.gz?download'>Alpha</a></li>";
         print "</ul></li>";
         
         // Huh?
         print "<li>That ... uhhh ... I guess they call it an \"operating system\" ... it's named ... uhhh ... doors ... or glass ... or something.<br>";
         print "<ul>";
-        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-i686-pc-cygwin.zip'>i686 (Intel/AMD PC (<b>NOT</b> Wintel you ignorant morons)</a></li>";
+        print "<li><a href='http://prdownloads.sourceforge.net/directnet/directnet-" . $ver . "-i686-pc-cygwin.zip?download'>i686 (Intel/AMD PC) (<b>NOT</b> Wintel you ignorant morons)</a></li>";
         print "</ul></li></ul>";
     }
 }
