baa397b54444a61826fb157d0cd4c745fcc34a03
cdgregorr <>
2005-01-11 19:03:21 +0000

    Maybe have chat windows now?
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk@112 40aaf297-f90e-0410-8da6-8adc2782f1e2

--- directnet/src/ui-fltk/ui.cc
+++ directnet/src/ui-fltk/ui.cc
@@ -21,15 +21,22 @@
 extern "C" {
 #include <pthread.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "connection.h"
 #include "directnet.h"
 #include "ui.h"
 }
 
+#include <iostream>
+
 #include "BuddyWindow.h"
+#include "ChatWindow.h
+
+ChatWindow *cws[1024];
+
+using namespace std;
 
 extern "C" int uiInit(int argc, char **argv, char **envp)
 {
@@ -37,40 +44,91 @@ extern "C" int uiInit(int argc, char **argv, char **envp)
     
     /* get the name */
     strcpy(dn_name, "Gregor");
+    
+    /* blank our array of windows */
+    memset(cws, 0, 1024 * sizeof(ChatWindow *));
 
+    /* make the buddy window */
     w = new BuddyWindow();
-    w->show();
+    w->make_window();
+    w->buddyWindow->show();
+
+    uiLoaded = 1;
     
     Fl::run();
 
     return 0;
 }
 
+ChatWindow *getWindow(char *name)
+{
+    int i;
+    
+    for (i = 0; cws[i] != NULL; i++) {
+        if (!strcmp(cws[i]->textOut->label(), name)) {
+            cws[i]->show();
+            return cws[i];
+        }
+    }
+    
+    cws[i] = new ChatWindow();
+    cws[i]->make_window();
+    cws[i]->chatWindow->label(name);
+    cws[i]->textOut->label(name);
+    cws[i]->show();
+    
+    return cws[i];
+}
+
 void estConn(Fl_Input *w, void *ignore)
 {
-    cout << "Connect to " << w->value() << endl;
+    char *connTo;
+    
+    connTo = strdup(w->value());
+    establishConnection(connTo);
+    free(connTo);
+}
+
+void sendInput(Fl_Input *w, void *ignore)
+{
 }
 
 extern "C" void uiDispMsg(char *from, char *msg)
 {
+    ChatWindow *cw;
+    char *dispmsg;
+    
+    while (!uiLoaded) sleep(0);
+    
+    dispmsg = alloca((strlen(from) + strlen(msg) + 4) * sizeof(char));
+    sprintf(dispmsg, "%s: %s\n", from, msg);
+    
+    cw = getWindow(from);
+    
+    cw->textOut->insert(dispmsg);
 }
 
 extern "C" void uiEstConn(char *from)
 {
+    while (!uiLoaded) sleep(0);
 }
 
 extern "C" void uiEstRoute(char *from)
 {
+    while (!uiLoaded) sleep(0);
 }
 
 extern "C" void uiLoseConn(char *from)
 {
+    while (!uiLoaded) sleep(0);
 }
 
 extern "C" void uiLoseRoute(char *from)
 {
+    while (!uiLoaded) sleep(0);
 }
 
 extern "C" void uiNoRoute(char *to)
 {
+    while (!uiLoaded) sleep(0);
 }
