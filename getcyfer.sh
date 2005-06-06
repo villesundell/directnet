06385751d249fb9dd09acd315ac7b7d3b597723c
cdgregorr <>
2005-06-06 15:51:33 +0000

    A convenient script for downloading Cyfer.
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk/directnet@217 40aaf297-f90e-0410-8da6-8adc2782f1e2

---
+++ getcyfer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd "`dirname $0`"
+cd src/enc-cyfer
+
+wget ftp://ftp.gnu.org/gnu/gmp/gmp-4.1.4.tar.bz2 || exit 1
+bunzip2 -c gmp-4.1.4.tar.bz2 | tar -xf - || exit 1
+mv gmp-4.1.4 gmp
+
+wget http://software.senko.net/projects/cyfer/sources/cyfer-0.6.0.tar.gz || exit 1
+gunzip -c cyfer-0.6.0.tar.gz | tar -xf - || exit 1
+mv cyfer-0.6.0 cyfer
+
+cd cyfer
+patch -p1 < ../cyfer-0.6.0.dn.diff
