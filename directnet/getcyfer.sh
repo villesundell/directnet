a33840dfef52af3a9e0b65265973a16fe70d5f39
nesius <>
2005-06-07 04:37:58 +0000

    Added --passive-ftp to the wgets to be more portable.
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk@228 40aaf297-f90e-0410-8da6-8adc2782f1e2

--- directnet/getcyfer.sh
+++ directnet/getcyfer.sh
@@ -2,11 +2,11 @@
 cd "`dirname $0`"
 cd src/enc-cyfer
 
-wget ftp://ftp.gnu.org/gnu/gmp/gmp-4.1.4.tar.bz2 || exit 1
+wget --passive-ftp ftp://ftp.gnu.org/gnu/gmp/gmp-4.1.4.tar.bz2 || exit 1
 bunzip2 -c gmp-4.1.4.tar.bz2 | tar -xf - || exit 1
 mv gmp-4.1.4 gmp
 
-wget http://software.senko.net/projects/cyfer/sources/cyfer-0.6.0.tar.gz || exit 1
+wget --passive-ftp http://software.senko.net/projects/cyfer/sources/cyfer-0.6.0.tar.gz || exit 1
 gunzip -c cyfer-0.6.0.tar.gz | tar -xf - || exit 1
 mv cyfer-0.6.0 cyfer
 
