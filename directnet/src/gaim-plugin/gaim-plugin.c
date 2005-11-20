e69c68705af11e29d8688a7e91c581285032a279
cdgregorr <>
2005-11-20 23:04:48 +0000

    Preparing for 1.0.0rc2.
    git-svn-id: svn://svn.code.sf.net/p/directnet/code/trunk@309 40aaf297-f90e-0410-8da6-8adc2782f1e2

--- directnet/src/gaim-plugin/gaim-plugin.c
+++ directnet/src/gaim-plugin/gaim-plugin.c
@@ -138,7 +138,7 @@ static GaimPluginInfo info = {
     
     "prpl-directnet", /* id */
     "DirectNet", /* name */
-    "1.0.0rc1", /* version */
+    "1.0.0rc2", /* version */
     
     N_("DirectNet Protocol Plugin"), /* summary */
     N_("DirectNet Protocol Plugin"), /* full ;) description */
